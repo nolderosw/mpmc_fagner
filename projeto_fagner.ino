3b1fd4566cb81633f95816525cb726f9cfdc214d
Wesley Azevedo <wesley150@air-de-wesley.home>
2018-02-28 20:23:11 -0300

    final temp. control arduino

--- projeto_fagner.ino
+++ projeto_fagner.ino
@@ -1,29 +1,57 @@
 
 #include <LiquidCrystal.h>
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+LiquidCrystal lcd(12, 11, 7, 6, 5, 4);
 const int LM35 = 0;
 float temperatura_atual = 0;
 int ADClido = 0;
-float temperatura_setada = 5;
+float temperatura_setada = 30;
+const int led_vermelho = 8;
+const int led_verde = 9;
 void setup(){
+ pinMode(led_vermelho, OUTPUT);
+ pinMode(led_verde, OUTPUT);
+
  Serial.begin(115200);
  analogReference(INTERNAL); //Se estiver usando Arduino Mega, use INTERNAL1V1
+ attachInterrupt(digitalPinToInterrupt(2),aumenta_temp,RISING);
+ attachInterrupt(digitalPinToInterrupt(3),diminui_temp,RISING);
 }
 void loop(){
  ADClido = analogRead(LM35);
  temperatura_atual = ADClido * 0.1075268817204301;
+ if(temperatura_atual >= temperatura_setada + 2){
+    //esfria
+    digitalWrite(led_verde, HIGH);
+    digitalWrite(led_vermelho, LOW);
+ }
+ else if(temperatura_atual <=  temperatura_setada - 2){
+    //aquece
+    digitalWrite(led_verde, LOW);
+    digitalWrite(led_vermelho, HIGH); 
+ }
+ else{
+    //tudo desligado
+    digitalWrite(led_verde, LOW);
+    digitalWrite(led_vermelho, LOW);
+ }
  delay(100);
- Serial.print(temperatura_atual);
  lcd.begin(16, 2);
  lcd.setCursor(0,0);
- lcd.print("Atual : ");
+ lcd.print("Atual: ");
  lcd.print(temperatura_atual);
  lcd.print(" *C");
  lcd.setCursor(0,1);
- lcd.print("Setada : ");
+ lcd.print("Setada: ");
  lcd.print(temperatura_setada);
  lcd.print(" *C");
  //Serial.print(float(temperatura_atual));
  //Serial.print(2);
  delay(1000);
+}
+
+void aumenta_temp(){ 
+  temperatura_setada++;
+}
+void diminui_temp(){ 
+  temperatura_setada--;
 }
--- web/app.js
+++ web/app.js
@@ -8,12 +8,14 @@ var io = require("socket.io")(http);
 
 app.get("/", function(req, res){
     res.sendFile("index.html");
-})
+}) 
+
+
 
 var mySocket;
-
 var serialport = require("serialport"); //incluindo o modulo de comunicação serial
 var SerialPort = serialport.SerialPort; //instanciando o objeto serial port
+
 
 var mySerial = new serialport("/dev/cu.usbserial-A403F0RK", {
     baudRate: 115200,
@@ -38,10 +40,30 @@ mySerial.on("data", function(data){
     data_anterior = parseFloat(data);
 }); //ocorre quando receber algum dado na serial
 
-io.on("conection", function(socket){
-    console.log("Um usuario entrou");
-})
-
 http.listen(3000, function (){
     console.log("Servidor rodando na porta 3000");
 })
+
+
+/* johnny five caso queira usar no futuro!!!
+var five = require("johnny-five");
+var board = new five.Board({ port: "/dev/cu.usbserial-A403F0RK", timeout: 3600 });
+
+board.on("ready", function() {
+
+  // Create a new generic sensor instance for
+  // a sensor connected to an analog (ADC) pin
+  var temperature = new five.Thermometer({
+    controller: "LM35",
+    pin: "A0"
+  });
+
+  temperature.on("change", function() {
+    console.log(this.celsius + "°C");
+    var temp = this.celsius;
+    io.emit("dadoArduino", {
+        valor: parseFloat(temp),
+    });
+  }),1000;
+});
+*/
